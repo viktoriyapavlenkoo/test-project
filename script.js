576858c430dadcdaea758add71bd3368ac3e4939
Viktoriya <viktoriyapavlenkoo418@gmail.com>
2024-04-09 15:52:49 +0300

    Added adaptive to footer section.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const openMenu = document.querySelector('.open-menu');
 const smartfonMenu = document.querySelector('.smartfon-menu')
 const closeMenu = document.querySelector('.close-menu');
 
+const infoBlock = document.querySelector('.footer__info-block')
+const info = document.querySelector('.info-block__list')
+
+const contactBlock = document.querySelector('.footer__contact-block')
+const address = document.querySelector('.footer__address')
 
 openMenu.onclick = function() {
     smartfonMenu.style.display = 'flex';    
@@ -10,4 +15,11 @@ openMenu.onclick = function() {
 closeMenu.onclick = function() {
     this.style.display = 'none';
     smartfonMenu.style.display = 'none';    
+}
+
+contactBlock.onclick = function() {
+    address.classList.toggle('open')
+}
+infoBlock.onclick = function() {
+    info.classList.toggle('open')
 }
